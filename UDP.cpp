ddf53681b07e4fcf4d5ec1ff987d744ed07ade12
Count Zero <king.seth@gmail.com>
2017-01-23 22:00:25 -0500

    continuing to port structs over to C++ classes working on UDP class.

---
+++ UDP.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 1/22/17.
+//
+
+#include "UDP.h"
---
+++ UDP.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 1/22/17.
+//
+
+#ifndef FREERTPS_UDP_H
+#define FREERTPS_UDP_H
+
+
+class UDP {
+
+};
+
+
+#endif //FREERTPS_UDP_H
